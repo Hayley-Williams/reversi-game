ea4d023cd33f85d10e0dbf42fc5225f75bd27c73
Hayley Williams <hayleyw@uci.edu>
2020-05-15 16:15:05 -0700

    created main.js, edited lobby.html

---
+++ public/main.js
@@ -0,0 +1,16 @@
+/* Functions for general use */
+/* This function returns the value associated with 'whichParam' on the URL */
+
+fucntion getURLParameters(whichParam)
+{
+  var pageURL = window.location.search.substring(1);
+  var pageURLVariables = pageURL.split('&');
+  for(var i = 0; i < pageURLVariables.length; i++){
+    var parameterName = pageURLVariables[i].split('=');
+    if (parameterName[0] == whichParam){
+      return parameterName[1];
+    }
+  }
+}
+
+$('#messages').append('<h4>' +getURLParameters('username')+'</h4'>);
